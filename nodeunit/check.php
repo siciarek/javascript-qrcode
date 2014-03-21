b0505989c88a14e633840d8afb62ea8de0e11c08
siciarek <siciarek@gmail.com>
2014-03-21 09:30:52 +0100

    Add encode decode unit tests

---
+++ nodeunit/check.php
@@ -0,0 +1,14 @@
+<?php
+
+$tempdir = $argv[1];
+$count = $argv[2];
+
+$actual = [];
+
+for($p = 0; $p < $count; $p++) {
+    $cmd = sprintf('zbarimg --quiet --raw %s/%d.pbm', $tempdir, $p);
+    $ret = `$cmd`;
+    $actual[] = trim($ret);
+}
+
+echo json_encode($actual);
