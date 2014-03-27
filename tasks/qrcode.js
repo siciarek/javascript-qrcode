071a986956c3876d6df6a108931fa64c5af85114
siciarek <siciarek@gmail.com>
2014-03-27 14:13:26 +0100

    Make tidy

--- tasks/qrcode.js
+++ tasks/qrcode.js
@@ -2589,415 +2589,72 @@ var Tiler = function (matrix) {
     'use strict';
 
     this.matrix = matrix;
-
-    this.cursor = {
-        x: this.matrix.getSize() - 1,
-        y: this.matrix.getSize() - 1
-    };
-
-    this.data = this.matrix.getData();
 };
 
 Tiler.prototype.constructor = Tiler;
 
-Tiler.prototype.writeBit = function (bit) {
-    'use strict';
-
-    if (bit === 1) {
-        this.matrix.setDarkModule(this.cursor.x, this.cursor.y, this.matrix.MASK_DATA);
-    }
-    else {
-        this.matrix.setLightModule(this.cursor.x, this.cursor.y, this.matrix.MASK_DATA);
-    }
-
-    if(this.cursor.x === 7 && this.cursor.y === 9) {
-        throw '7-9';
-    }
-};
-
 Tiler.prototype.setArea = function (datastr) {
     'use strict';
 
-    // Bits array:
-    var bits = datastr.split('').map(function (e) {
-        return parseInt(e);
-    });
+    var data = datastr.split('').parseInt();
 
-    var index = 0;
-    var offix, bit;
-    var limit = bits.length;
+    var UP = 0;
+    var DOWN = 1;
 
-    limit -= 1;
+    var direction = UP;
 
-    var tempy = 0;
-
-    var offsets = [
-        [
-            [-1, 0],
-            [1, -1]
-        ],
-        [
-            [-1, 0],
-            [1, 1]
-        ]
+    var offsets = {};
+    offsets[UP] = [
+        [-1, 0],
+        [1, -1]
+    ];
+    offsets[DOWN] = [
+        [-1, 0],
+        [1, 1]
     ];
 
-    try {
-        while (bits.length > 0) {
+    // Start at the last module:
+    var x = this.matrix.getSize() - 1;
+    var y = this.matrix.getSize() - 1;
+    var tempy = y;
 
-            if (index >= limit) {
-                break;
-            }
+    while (data.length > 0) {
 
-            offix = 0;
+        // Check if matrix bottom or top is reached:
+        if (!(y >= 0 && y < this.matrix.getSize())) {
+            x -= 2;
+            y = tempy;
 
-            while (bits.length > 0) {
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][0][0];
-                this.cursor.y += offsets[offix][0][1];
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][1][0];
-                this.cursor.y += offsets[offix][1][1];
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][0][0];
-                this.cursor.y += offsets[offix][0][1];
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][1][0];
-                this.cursor.y += offsets[offix][1][1];
-            }
-
-            if (index >= limit) {
-                break;
-            }
-
-            this.cursor.x += -2;
-            this.cursor.y = tempy;
-
-            offix = 1;
-
-            while (bits.length > 0) {
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][0][0];
-                this.cursor.y += offsets[offix][0][1];
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][1][0];
-                this.cursor.y += offsets[offix][1][1];
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][0][0];
-                this.cursor.y += offsets[offix][0][1];
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][1][0];
-                this.cursor.y += offsets[offix][1][1];
-            }
-
-            this.cursor.x += -2;
-            this.cursor.y = tempy;
+            direction += 1;
+            direction %= 2;
         }
 
-    }
-    catch(e) {
-        this.cursor.x += -2;
+        for (var i = 0; i < offsets[direction].length; i += 1) {
 
-        while (bits.length > 0) {
+            // Place data bit only module is undefined:
+            if (this.matrix.data[y][x] === this.matrix.DATA_UNDEFINED_MODULE) {
 
-            if (index >= limit) {
-                break;
+                var bit = data.shift();
+
+                if (bit === 1) {
+                    this.matrix.setDarkModule(x, y, this.matrix.MASK_DATA);
+                }
+                else {
+                    this.matrix.setLightModule(x, y, this.matrix.MASK_DATA);
+                }
+
+                // Check if { x: 7, y: 9 } module is reached:
+                if (x === 7 && y === 9) {
+                    x -= 2;
+                    direction = DOWN;
+                    break;
+                }
             }
 
-            offix = 1;
+            tempy = y;
 
-            while (bits.length > 0) {
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][0][0];
-                this.cursor.y += offsets[offix][0][1];
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][1][0];
-                this.cursor.y += offsets[offix][1][1];
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][0][0];
-                this.cursor.y += offsets[offix][0][1];
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][1][0];
-                this.cursor.y += offsets[offix][1][1];
-            }
-
-            if (index >= limit) {
-                break;
-            }
-
-            this.cursor.x += -2;
-            this.cursor.y = tempy;
-
-            offix = 0;
-
-            while (bits.length > 0) {
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][0][0];
-                this.cursor.y += offsets[offix][0][1];
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][1][0];
-                this.cursor.y += offsets[offix][1][1];
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][0][0];
-                this.cursor.y += offsets[offix][0][1];
-
-                if (typeof this.data[this.cursor.y] === 'undefined' || this.data[this.cursor.y][this.cursor.x] === 'undefined') {
-                    break;
-                }
-
-
-                if (this.data[this.cursor.y][this.cursor.x] === this.matrix.DATA_UNDEFINED_MODULE) {
-                    bit = bits.shift();
-                    this.writeBit(bit);
-
-                    index += 1;
-                    if (index >= limit) {
-                        break;
-                    }
-                }
-
-                tempy = this.cursor.y;
-                this.cursor.x += offsets[offix][1][0];
-                this.cursor.y += offsets[offix][1][1];
-            }
-
-            this.cursor.x += -2;
-            this.cursor.y = tempy;
+            x += offsets[direction][i][0];
+            y += offsets[direction][i][1];
         }
     }
 };
