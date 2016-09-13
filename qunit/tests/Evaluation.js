0a51f31226b92ee276d51e10bd98c4989d3549a9
JerryShen <jerry82422@gmail.com>
2016-09-13 14:55:03 +0800

    chore: Remove unused console.log

--- qunit/tests/Evaluation.js
+++ qunit/tests/Evaluation.js
@@ -6,7 +6,6 @@ test('Evaluation Test', function () {
     while(evaluationRoleDataProvider.length > 0) {
         test = evaluationRoleDataProvider.shift();
         expected = test.result;
-        console.log(JSON.stringify(test));
         actual = evaluation.rules[test.rule](test.data);
 
         deepEqual(actual, expected, ['Evaluation Rule #' + test.rule].toString());
